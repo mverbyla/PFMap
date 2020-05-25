bb4a6babf1eea7389b786f0af83711cc37e2bac4
Matthew Verbyla <matthewverbyla@Matthews-iMac.local>
2020-05-25 14:59:43 -0700

    hi

--- R/plotPathogenFlows.R
+++ R/plotPathogenFlows.R
@@ -8,8 +8,8 @@
 #' plotPathogenFlows(getLRV(mySketch="http://data.waterpathogens.org/dataset/afafd87c-b592-44c5-bb42-0a5a8415e54b/resource/83057ee9-402d-4b9b-8ab3-92053ee94c63/download/lubigisewageandfecalsludgetreatmentsystemv4.json",pathogenType="Virus",inFecalSludge=10000000000,inSewage=10000000000))
 #'
 plotPathogenFlows<-function(sketcherResults){
-  require(igraph)
-  require(networkD3)
+  #require(igraph)
+  #require(networkD3)
   myNodes<-data.frame(name=make.names(sketcherResults$nodes$subType,unique=T),id=nodes$name)
   myNodes$newID<-rownames(myNodes)
   myLinks<-data.frame(source=sketcherResults$arrows$us_node,target=sketcherResults$arrows$ds_node,value=sketcherResults$arrows$relativeLoading*100)
